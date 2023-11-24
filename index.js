843df3a089f6b3a47e0a43a25f838a64db5b71b5
Đạt Sài <nguyensaithanhdat@gmail.com>
2023-11-24 11:27:01 +0700

    update name file

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('express');
+const app = express();
+const serverless = require('serverless-http');
+// const dotenv = require("dotenv");
+const PORT = 3000;
+const Redis = require("redis");
+const client = Redis.createClient();
+const { checkExistUser } = require('./middlewares')
+var mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const categoryRote = require("./routes/category");
+ 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
-app.listen(process.env.PORT || 3000)
+// app.use(express.json());
+
+
+/// redisInit 
+client.connect();
+global.cached = client;
+
+// dotenv.config();
+
+/// connect with mongo
+mongoose.connect("mongodb+srv://phamtrithuc22022000:oAdzE3Vz4aKI89D7@cluster0.eq2bixo.mongodb.net/").then(() => console.log("DB connection successful")).catch((err) => {
+    console.log(err);
+});
+
+/// using json express
+
+app.listen(PORT, () => {
+    console.log(`Your server is running at port: ${PORT}`)
+})
+
+app.use("/api/auth", checkExistUser, authRoute);
+app.use("/api/product", productRoute);
+app.use("/api/category", categoryRote);
+
+
+module.exports.handler = serverless(app);
